86eaf0669a1858db3ab1247080b5658fb966d45e
Ohad Lutzky <ohad@lutzky.net>
2022-04-10 22:23:34 +0100

    Basic keyboard controls

--- main.go
+++ main.go
@@ -17,22 +17,68 @@ type Game struct {
 	vY float64
 }
 
+const (
+	maxVX    = 5
+	friction = 0.05
+)
+
+var (
+	playerColor = color.RGBA{0, 0, 255, 255}
+	isOnFloor   = true
+	jumpPressed = false
+)
+
+func (g *Game) handleInput() {
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		g.vX += 0.1
+	} else if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		g.vX -= 0.1
+	} else {
+		g.vX *= (1 - friction)
+	}
+	if g.vX > maxVX {
+		g.vX = maxVX
+	} else if g.vX < -maxVX {
+		g.vX = -maxVX
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeySpace) {
+		if !jumpPressed {
+			jumpPressed = true
+			if isOnFloor {
+				g.vY = 10
+			}
+		}
+	} else {
+		jumpPressed = false
+	}
+}
+
 func (g *Game) Update() error {
+	g.handleInput()
 	g.RectX += g.vX
 	g.RectY += g.vY
-	g.vY -= 1.0
-	g.vY *= 0.99
-	if g.RectY < 0 {
-		g.vY = -g.vY
+	if g.RectY <= 0 {
+		g.vY = 0
+		g.RectY = 0
+		isOnFloor = true
+	} else {
+		isOnFloor = false
+		g.vY -= 1.0
+		g.vY *= 0.99
 	}
-	if g.RectX > 320-30 || g.RectX < 0 {
+	if g.RectX > 320-30 {
+		g.RectX = 320 - 30
+		g.vX = -g.vX
+	} else if g.RectX < 0 {
+		g.RectX = 0
 		g.vX = -g.vX
 	}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DrawRect(screen, g.RectX, 240-30-g.RectY, 30, 30, color.White)
+	ebitenutil.DrawRect(screen, g.RectX, 240-30-g.RectY, 30, 30, playerColor)
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("Pos: (%3.0f,%3.0f) V: (%3.1f,%3.1f)", g.RectX, g.RectY, g.vX, g.vY))
 }
 
@@ -49,7 +95,7 @@ func main() {
 	if err := ebiten.RunGame(&Game{
 		RectX: 30,
 		RectY: 100,
-		vX:    3.5,
+		vX:    0,
 	}); err != nil {
 		log.Fatal(err)
 	}
