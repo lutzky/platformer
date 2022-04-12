aa59c8d7ba926b60ca5400133c051354ac74da85
Ohad Lutzky <ohad@lutzky.net>
2022-04-12 21:22:09 +0100

    Move Player into its own module

--- main.go
+++ main.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 	"image/color"
 	"log"
-	"math"
 	"os"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 
+	playerlib "github.com/lutzky/platformer/player"
 	"github.com/lutzky/platformer/rectangle"
-	"github.com/lutzky/platformer/sprites"
 
 	_ "embed"
 	_ "image/png"
@@ -25,106 +25,7 @@ var game = Game{
 	gravity: 0.8,
 }
 
-type Player struct {
-	vX, vY float64
-	rect   rectangle.Rectangle[float64]
-
-	isOnFloor, isJumping         bool
-	color, colorJump, colorFloor color.Color
-
-	friction, acceleration float64
-	maxVX                  float64
-	terminalVelocityY      float64
-
-	jumpSpeed      float64
-	jumpHoverSpeed float64
-
-	scaling       float64
-	scalingFactor float64 // TODO(lutzky): Rename me
-
-	marginTop, marginLeft, marginBottom, marginRight float64
-}
-
-var player = Player{
-	color:      color.RGBA{0, 0, 255, 255},
-	colorJump:  color.RGBA{255, 0, 255, 255},
-	colorFloor: color.RGBA{0, 255, 255, 255},
-
-	rect: rectangle.Rect[float64](0, 0, 32, 32),
-
-	friction:          0.08,
-	acceleration:      0.3,
-	maxVX:             5,
-	terminalVelocityY: 9,
-
-	jumpSpeed:      12,
-	jumpHoverSpeed: 3,
-
-	scaling:       1,
-	scalingFactor: 1, // 0.9, TODO(lutzky): Set me back to 0.9
-
-	marginTop:    6,
-	marginLeft:   6,
-	marginRight:  6,
-	marginBottom: 0,
-}
-
-func (p *Player) SetLeft(x float64) {
-	p.rect.SetLeft(x - p.marginLeft)
-}
-func (p *Player) SetRight(x float64) {
-	p.rect.SetRight(x + p.marginRight)
-}
-func (p *Player) SetTop(y float64) {
-	p.rect.SetTop(y - p.marginTop)
-}
-func (p *Player) SetBottom(y float64) {
-	p.rect.SetBottom(y + p.marginBottom)
-}
-
-func (player *Player) hitbox() rectangle.Rectangle[float64] {
-	return rectangle.Rect(
-		player.rect.Min.X+player.marginLeft,
-		player.rect.Min.Y+player.marginTop,
-		player.rect.Max.X-player.marginRight,
-		player.rect.Max.Y-player.marginBottom,
-	)
-}
-
-func (player *Player) draw(dst *ebiten.Image) {
-	var sprite *sprites.Sprite
-	switch {
-	case player.vY < -0.2:
-		sprite = sprites.Jump
-	case player.vY > 0.2:
-		sprite = sprites.Fall
-	case math.Abs(player.vX) > 0.2:
-		sprite = sprites.Run
-	default:
-		sprite = sprites.Idle
-	}
-
-	op := &ebiten.DrawImageOptions{}
-	if player.vX < 0 {
-		op.GeoM.Scale(-1, 1)
-		op.GeoM.Translate(32, 0)
-	}
-	op.GeoM.Scale(player.scaling, player.scaling)
-	op.GeoM.Translate(player.rect.Min.X, player.rect.Min.Y)
-	dst.DrawImage(sprite.GetFrame(), op)
-	if debug {
-		player.drawHitbox(dst)
-	}
-}
-
-func (player *Player) drawHitbox(dst *ebiten.Image) {
-	hb := player.hitbox()
-	c := color.White
-	ebitenutil.DrawLine(dst, hb.Min.X, hb.Min.Y, hb.Max.X, hb.Min.Y, c)
-	ebitenutil.DrawLine(dst, hb.Min.X, hb.Min.Y, hb.Min.X, hb.Max.Y, c)
-	ebitenutil.DrawLine(dst, hb.Min.X, hb.Max.Y, hb.Max.X, hb.Max.Y, c)
-	ebitenutil.DrawLine(dst, hb.Max.X, hb.Min.Y, hb.Max.X, hb.Max.Y, c)
-}
+var player = playerlib.New()
 
 const (
 	tileHeight   = 30
@@ -134,8 +35,7 @@ const (
 )
 
 var (
-	jumpPressed = false
-	debug       = false
+	debug = false
 )
 
 var tileMap = []string{
@@ -179,129 +79,102 @@ func (g *Game) handleInput() {
 		os.Exit(0)
 	}
 
-	debug = ebiten.IsKeyPressed(ebiten.KeyD)
+	if inpututil.IsKeyJustPressed(ebiten.KeyD) {
+		debug = !debug
+	}
 
 	var dVX float64
 	if ebiten.IsKeyPressed(ebiten.KeyRight) {
-		dVX = player.acceleration
+		dVX = player.Acceleration
 	} else if ebiten.IsKeyPressed(ebiten.KeyLeft) {
-		dVX = -player.acceleration
+		dVX = -player.Acceleration
 	}
 
-	if dVX*player.vX <= 0 {
+	if dVX*player.VX <= 0 {
 		// Apply friction unless accelerating in the current movement direction
-		player.vX *= (1 - player.friction)
+		player.VX *= (1 - player.Friction)
 	}
-	player.vX += dVX
+	player.VX += dVX
 
-	if player.vX > player.maxVX {
-		player.vX = player.maxVX
-	} else if player.vX < -player.maxVX {
-		player.vX = -player.maxVX
+	if player.VX > player.MaxVX {
+		player.VX = player.MaxVX
+	} else if player.VX < -player.MaxVX {
+		player.VX = -player.MaxVX
 	}
 
-	if player.vY >= 0 {
-		player.isJumping = false
-	}
-	if ebiten.IsKeyPressed(ebiten.KeySpace) || ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
-		if !jumpPressed {
-			jumpPressed = true
-			if player.isOnFloor {
-				player.vY = -player.jumpSpeed
-				player.isJumping = true
-			}
-		}
-	} else {
-		jumpPressed = false
-		if player.isJumping && player.vY < -player.jumpHoverSpeed {
-			player.vY = -player.jumpHoverSpeed
-		}
-		player.isJumping = false
-	}
+	player.HandleJump(ebiten.IsKeyPressed(ebiten.KeyUp))
 }
 
 func (g *Game) checkIsOnFloor() {
-	// TODO(lutzky): This uses hitbox inconsistently, and doesn't use rectangles
-	// for tiles
-	hb := player.hitbox()
+	hb := player.Hitbox()
 	for _, t := range tiles {
 		if hb.Max.X >= t.x && hb.Min.X <= t.x+tileWidth &&
 			hb.Max.Y == t.y {
-			player.isOnFloor = true
+			player.IsOnFloor = true
 			return
 		}
 	}
-	player.isOnFloor = false
+	player.IsOnFloor = false
 }
 
 func (g *Game) handleXCollisions() {
-	hitbox := player.hitbox()
+	hitbox := player.Hitbox()
 	for _, t := range tiles {
 		if hitbox.Overlaps(t.rect()) {
-			if player.vX > 0 {
+			if player.VX > 0 {
 				player.SetRight(t.rect().Min.X)
-				player.vX = 0
-			} else if player.vX < 0 {
+				player.VX = 0
+			} else if player.VX < 0 {
 				player.SetLeft(t.rect().Max.X)
-				player.vX = -0.01
+				player.VX = -0.01
 			}
 		}
-
-	}
-
-	if player.rect.Max.X > screenWidth {
-		player.rect.SetRight(screenWidth)
-		player.vX *= -1
-	} else if player.rect.Min.X < 0 {
-		player.rect.SetLeft(0)
-		player.vX *= -1
 	}
 }
 
 func (g *Game) handleYCollisions() {
 	for _, t := range tiles {
-		if player.hitbox().Overlaps(t.rect()) {
-			if player.vY > 0 {
+		if player.Hitbox().Overlaps(t.rect()) {
+			if player.VY > 0 {
 				player.SetBottom(t.rect().Min.Y)
 			} else {
 				player.SetTop(t.rect().Max.Y)
-				player.rect.Scale(player.scalingFactor)
-				player.scaling *= player.scalingFactor
+				player.Scale()
 			}
-			player.vY = 0
+			player.VY = 0
 		}
 	}
 }
 
 func (g *Game) applyGravity() {
-	if player.isOnFloor {
+	if player.IsOnFloor {
 		return
 	}
-	player.vY += g.gravity
-	if player.vY > player.terminalVelocityY {
-		player.vY = player.terminalVelocityY
+	player.VY += g.gravity
+	if player.VY > player.TerminalVelocityY {
+		player.VY = player.TerminalVelocityY
 	}
 }
 
 func (g *Game) Update() error {
 	g.checkIsOnFloor()
 	g.handleInput()
-	player.rect.MoveX(player.vX)
+	player.MoveX(player.VX)
 	g.handleXCollisions()
-	player.rect.MoveY(player.vY)
+	player.MoveY(player.VY)
 	g.applyGravity()
 	g.handleYCollisions()
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	player.draw(screen)
+	player.Draw(screen)
 	for _, t := range tiles {
 		ebitenutil.DrawRect(screen, t.x, t.y, tileWidth, tileHeight, color.White)
 	}
 	if debug {
-		ebitenutil.DebugPrint(screen, fmt.Sprintf("Pos: (%3.0f,%3.0f) V: (%3.1f,%3.1f), IoF: %t",
-			player.rect.Min.X, player.rect.Min.Y, player.vX, player.vY, player.isOnFloor))
+		ebitenutil.DebugPrint(screen, player.DebugString())
+		player.DrawHitbox(screen)
 	}
 }
 
---
+++ player/player.go
@@ -0,0 +1,156 @@
+package player
+
+import (
+	"fmt"
+	"image/color"
+	"math"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/lutzky/platformer/rectangle"
+	"github.com/lutzky/platformer/sprites"
+)
+
+type Player struct {
+	VX, VY float64
+	rect   rectangle.Rectangle[float64]
+
+	IsOnFloor, isJumping         bool
+	color, colorJump, colorFloor color.Color
+
+	Friction, Acceleration float64
+	MaxVX                  float64
+	TerminalVelocityY      float64
+
+	jumpSpeed      float64
+	jumpHoverSpeed float64
+	jumpStarted    bool
+
+	scaling       float64
+	scalingFactor float64 // TODO(lutzky): Rename me
+
+	marginTop, marginLeft, marginBottom, marginRight float64
+}
+
+func New() Player {
+	return Player{
+		color:      color.RGBA{0, 0, 255, 255},
+		colorJump:  color.RGBA{255, 0, 255, 255},
+		colorFloor: color.RGBA{0, 255, 255, 255},
+
+		rect: rectangle.Rect[float64](0, 0, 32, 32),
+
+		Friction:          0.08,
+		Acceleration:      0.3,
+		MaxVX:             5,
+		TerminalVelocityY: 9,
+
+		jumpSpeed:      12,
+		jumpHoverSpeed: 3,
+
+		scaling:       1,
+		scalingFactor: 1, // 0.9, TODO(lutzky): Set me back to 0.9
+
+		marginTop:    6,
+		marginLeft:   6,
+		marginRight:  6,
+		marginBottom: 0,
+	}
+}
+
+func (p *Player) SetLeft(x float64) {
+	p.rect.SetLeft(x - p.marginLeft)
+}
+
+func (p *Player) SetRight(x float64) {
+	p.rect.SetRight(x + p.marginRight)
+}
+
+func (p *Player) SetTop(y float64) {
+	p.rect.SetTop(y - p.marginTop)
+}
+
+func (p *Player) SetBottom(y float64) {
+	p.rect.SetBottom(y + p.marginBottom)
+}
+
+func (p *Player) MoveX(dX float64) {
+	p.rect.MoveX(dX)
+}
+
+func (p *Player) MoveY(dY float64) {
+	p.rect.MoveY(dY)
+}
+
+// TODO(lutzky): Maybe unexport?
+func (p *Player) Hitbox() rectangle.Rectangle[float64] {
+	return rectangle.Rect(
+		p.rect.Min.X+p.marginLeft,
+		p.rect.Min.Y+p.marginTop,
+		p.rect.Max.X-p.marginRight,
+		p.rect.Max.Y-p.marginBottom,
+	)
+}
+
+func (p *Player) Draw(dst *ebiten.Image) {
+	var sprite *sprites.Sprite
+	switch {
+	case p.VY < -0.2:
+		sprite = sprites.Jump
+	case p.VY > 0.2:
+		sprite = sprites.Fall
+	case math.Abs(p.VX) > 0.2:
+		sprite = sprites.Run
+	default:
+		sprite = sprites.Idle
+	}
+
+	op := &ebiten.DrawImageOptions{}
+	if p.VX < 0 {
+		op.GeoM.Scale(-1, 1)
+		op.GeoM.Translate(32, 0)
+	}
+	op.GeoM.Scale(p.scaling, p.scaling)
+	op.GeoM.Translate(p.rect.Min.X, p.rect.Min.Y)
+	dst.DrawImage(sprite.GetFrame(), op)
+}
+
+func (p *Player) DrawHitbox(dst *ebiten.Image) {
+	hb := p.Hitbox()
+	c := color.White
+	ebitenutil.DrawLine(dst, hb.Min.X, hb.Min.Y, hb.Max.X, hb.Min.Y, c)
+	ebitenutil.DrawLine(dst, hb.Min.X, hb.Min.Y, hb.Min.X, hb.Max.Y, c)
+	ebitenutil.DrawLine(dst, hb.Min.X, hb.Max.Y, hb.Max.X, hb.Max.Y, c)
+	ebitenutil.DrawLine(dst, hb.Max.X, hb.Min.Y, hb.Max.X, hb.Max.Y, c)
+}
+
+func (p *Player) HandleJump(jumpKeyDown bool) {
+	if p.VY >= 0 {
+		p.isJumping = false
+	}
+	if jumpKeyDown {
+		if !p.jumpStarted {
+			p.jumpStarted = true
+			if p.IsOnFloor {
+				p.VY = -p.jumpSpeed
+				p.isJumping = true
+			}
+		}
+	} else {
+		p.jumpStarted = false
+		if p.isJumping && p.VY < -p.jumpHoverSpeed {
+			p.VY = -p.jumpHoverSpeed
+		}
+		p.isJumping = false
+	}
+}
+
+func (p *Player) Scale() {
+	p.rect.Scale(p.scalingFactor)
+	p.scaling *= p.scalingFactor
+}
+
+func (p *Player) DebugString() string {
+	return fmt.Sprintf("Pos: (%3.0f,%3.0f) V: (%3.1f,%3.1f)",
+		p.rect.Min.X, p.rect.Min.Y, p.VX, p.VY)
+}
